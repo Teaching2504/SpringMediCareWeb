badd22688bc316cea3d8041c0b64a8721fde2d2f
kimphung <kimphungduongle9@gmail.com>
2026-04-12 09:28:30 +0700

    Update database medicare_db.sql

--- medicare_db.sql
+++ medicare_db.sql
@@ -3,8 +3,7 @@
 -- Host: localhost    Database: medicare_db
 -- ------------------------------------------------------
 -- Server version	8.0.45
-CREATE DATABASE medicare_db;
-USE medicare_db;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
